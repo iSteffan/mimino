bf6c45b3f16de609ade5e4a941ab1eaa5b2a9351
Stepan Skichko <st.skichko@gmail.com>
2024-08-15 13:32:39 +0300

    add room descr mobile markup // except 1 btn

--- sections/RoomSections/RoomSection.tsx
+++ sections/RoomSections/RoomSection.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+
+import { Btn } from '@/components/common/Btn';
+
+import roomData from '@/data/room.json';
+
 import { IRoomSection } from './type';
 
 export const RoomSection = ({ room }: IRoomSection) => {
@@ -10,9 +15,36 @@ export const RoomSection = ({ room }: IRoomSection) => {
     <section className="bg-lemonBg py-[32px]">
       <div className="container">
         <h2 className="mb-[16px] font-times text-[32px] font-400">{desc.amenities.name}</h2>
-        <p className="font-mont font-700 text-[16px] tracking-[0.32px] text-[#7a7a7a]">
-          {desc.title}
-        </p>
+        <p className="roomDescText">{desc.title}</p>
+
+        <h3 className="mt-[32px] mb-[16px] font-times font-700 text-[24px]">{desc.general.name}</h3>
+        <p className="roomDescText">{desc.general.text}</p>
+
+        <h3 className="mt-[32px] mb-[16px] font-times font-700 text-[24px]">
+          {desc.bathroom.name}
+        </h3>
+        <p className="roomDescText">{desc.bathroom.text}</p>
+
+        <h3 className="mt-[32px] mb-[16px] font-times font-700 text-[24px]">{desc.bedroom.name}</h3>
+        <p className="roomDescText">{desc.bedroom.text}</p>
+
+        <h3 className="mt-[32px] mb-[16px] font-times font-700 text-[24px]">{desc.kitchen.name}</h3>
+        <p className="roomDescText">{desc.kitchen.text}</p>
+
+        <h3 className="mt-[32px] mb-[16px] font-times font-700 text-[24px]">{desc.media.name}</h3>
+        <p className="roomDescText">{desc.media.text}</p>
+
+        <Btn type="button" isBtn classnameProps="mt-[32px] py-[20px] bg-black w-full text-[20px]">
+          {roomData.reserve}
+        </Btn>
+
+        <Btn
+          isLink
+          linkTo={roomData.goBack.link}
+          classnameProps="mt-[20px] w-full bg-none border-white hover:border-white"
+        >
+          {roomData.goBack.name}
+        </Btn>
       </div>
     </section>
   );
