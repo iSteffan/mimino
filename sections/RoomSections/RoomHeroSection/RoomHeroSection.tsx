9b5ca1fc57899d7af03cef3d4569ff485c36c29e
Stepan Skichko <st.skichko@gmail.com>
2024-08-30 14:08:19 +0300

    add basic address & socials to roomHero

--- sections/RoomSections/RoomHeroSection/RoomHeroSection.tsx
+++ sections/RoomSections/RoomHeroSection/RoomHeroSection.tsx
@@ -1,9 +1,11 @@
 import { RoomSlider } from '@/components/common/RoomSlider';
 import { Btn } from '@/components/common/Btn';
+import { ContactAddress } from '@/components/common/ContactAddress';
 
 import roomData from '@/data/room.json';
 
 import { IRoomHero } from './type';
+import { Socials } from '@/components/common/Socials';
 
 export const RoomHeroSection = ({ sliderData }: IRoomHero) => {
   return (
@@ -12,6 +14,11 @@ export const RoomHeroSection = ({ sliderData }: IRoomHero) => {
       <Btn isBtn type="button">
         {roomData.checkPrice}
       </Btn>
+      <ContactAddress
+        isHero
+        classnameProps="absolute top-[64px] right-0 z-20 flex md:relative md:h-[56px] md:top-0"
+      />
+      <Socials classnameProps="flex-col gap-[8px]" />
     </div>
   );
 };
