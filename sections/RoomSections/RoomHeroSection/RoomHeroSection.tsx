25b07980383fda8990c00df494557758b741e68f
Stepan Skichko <st.skichko@gmail.com>
2024-09-02 12:30:24 +0300

    little update

--- sections/RoomSections/RoomHeroSection/RoomHeroSection.tsx
+++ sections/RoomSections/RoomHeroSection/RoomHeroSection.tsx
@@ -1,24 +1,27 @@
 import { RoomSlider } from '@/components/common/RoomSlider';
 import { Btn } from '@/components/common/Btn';
 import { ContactAddress } from '@/components/common/ContactAddress';
+import { Socials } from '@/components/common/Socials';
 
 import roomData from '@/data/room.json';
 
 import { IRoomHero } from './type';
-import { Socials } from '@/components/common/Socials';
 
 export const RoomHeroSection = ({ sliderData }: IRoomHero) => {
   return (
     <div className="max-w-full mx-auto mt-[-64px]">
       <RoomSlider data={sliderData} classnameProps="relative" />
-      <Btn isBtn type="button">
-        {roomData.checkPrice}
-      </Btn>
-      <ContactAddress
-        isHero
-        classnameProps="absolute top-[64px] right-0 z-20 flex md:relative md:h-[56px] md:top-0"
-      />
-      <Socials classnameProps="flex-col gap-[8px]" />
+      <div className="container">
+        <Btn isBtn type="button">
+          {roomData.checkPrice}
+        </Btn>
+        <ContactAddress
+          isHero
+          // classnameProps="absolute top-[64px] right-0 z-20 flex md:relative md:h-[56px] md:top-0"
+          classnameProps="ml-auto flex md:relative md:h-[56px] md:top-0"
+        />
+        <Socials classnameProps="flex-col gap-[8px]" />
+      </div>
     </div>
   );
 };
