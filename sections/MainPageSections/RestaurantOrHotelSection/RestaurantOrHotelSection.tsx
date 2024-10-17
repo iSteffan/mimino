f70d6b21310072a907d42fb37829fdc0f38ee8d9
Stepan Skichko <st.skichko@gmail.com>
2024-10-17 14:46:13 +0300

    linked all the buttons on the main page to the form

--- sections/MainPageSections/RestaurantOrHotelSection/RestaurantOrHotelSection.tsx
+++ sections/MainPageSections/RestaurantOrHotelSection/RestaurantOrHotelSection.tsx
@@ -1,19 +1,40 @@
+'use client';
+
+import { useState } from 'react';
 import classNames from 'classnames';
 
 import { SectionTitleMimino } from '@/components/common/SectionTitleMimino';
 import { Slider } from '@/components/common/Slider';
-import { BtnList } from '@/components/common/BtnList';
+import { Modal } from '@/components/ui/Modal';
+import { Form } from '@/components/common/Form';
+import { Btn } from '@/components/common/Btn';
 
 import mainPageData from '@/data/mainPageData.json';
 
 import { IRestaurantOrHotelSection } from './type';
 
 export const RestaurantOrHotelSection = ({ isHotel, isRestaurant }: IRestaurantOrHotelSection) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [formTypeName, setFormTypeName] = useState<'table' | 'room' | 'roomPrice'>('table');
+
   const { restaurant, restaurantDesc, restaurantSlider, btnData, hotel, hotelDesc, hotelSlider } =
     mainPageData;
 
   const BtnData = isRestaurant ? btnData.restaurantSection : btnData.hotelSection;
   const SliderData = isRestaurant ? restaurantSlider : hotelSlider;
+
+  const handleToggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  const handleBtnClick = (type: string) => {
+    if (type === 'table' || type === 'room' || type === 'roomPrice') {
+      setFormTypeName(type);
+      handleToggleModal();
+    } else {
+      console.error(`Invalid type: ${type}`);
+    }
+  };
 
   const sectionStyles = classNames('pt-[24px] pb-[40px] bg-cover bg-center', {
     'restaurant-main-bg': isRestaurant,
@@ -52,13 +73,39 @@ export const RestaurantOrHotelSection = ({ isHotel, isRestaurant }: IRestaurantO
 
         <div className={flexContainerStyles}>
           <Slider section={isRestaurant ? 'mainRestaurant' : 'mainHotel'} data={SliderData} />
-          <BtnList
-            BtnData={BtnData}
-            listClassnameProps="gap-[16px] md:mt-[151px]"
-            btnClassnameProps="border-white rounded-[25px] border-[2px]"
-          />
+
+          <div className="flex flex-col gap-[16px] md:mt-[151px] smOnly:items-center">
+            <Btn
+              {...(BtnData[0].type === 'btn' && { type: 'button', isBtn: true })}
+              {...(BtnData[0].type === 'link' && { isLink: true, linkTo: BtnData[0].linkTo })}
+              classnameProps="border-white rounded-[25px] border-[2px]"
+              {...(BtnData[0].formType !== 'none'
+                ? { onClick: () => handleBtnClick(BtnData[0].formType) }
+                : {})}
+            >
+              {BtnData[0].text}
+            </Btn>
+            <Btn
+              {...(BtnData[1].type === 'btn' && { type: 'button', isBtn: true })}
+              {...(BtnData[1].type === 'link' && { isLink: true, linkTo: BtnData[1].linkTo })}
+              classnameProps="border-white rounded-[25px] border-[2px]"
+              {...(BtnData[1].formType !== 'none'
+                ? { onClick: () => handleBtnClick(BtnData[1].formType) }
+                : {})}
+            >
+              {BtnData[1].text}
+            </Btn>
+          </div>
         </div>
       </div>
+
+      <Modal open={isModalOpen} onClose={handleToggleModal}>
+        <Form
+          formTypeName={formTypeName}
+          onClose={handleToggleModal}
+          setFormTypeName={setFormTypeName}
+        />
+      </Modal>
     </section>
   );
 };
