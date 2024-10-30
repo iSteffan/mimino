61174e0b15e774955217b9231192ceedd2e8362c
Stepan Skichko <st.skichko@gmail.com>
2024-10-30 13:01:10 +0200

    start trials with sanity

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,6 +1,9 @@
 import page from './page-schema';
 import project from './project-schema';
+import { eventType } from './eventType';
+import { artistType } from './artistType';
+import { venueType } from './venueType';
 
-const schemas = [project, page];
+const schemas = [project, page, eventType, artistType, venueType];
 
 export default schemas;
