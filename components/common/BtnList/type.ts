ab65ab1f5a1cc3bb72ce582172d3276b7977c850
Stepan Skichko <st.skichko@gmail.com>
2024-11-12 13:09:35 +0200

    delete BtnList

--- components/common/BtnList/type.ts
+++
@@ -1,5 +0,0 @@
-export interface IBtnList {
-  BtnData: { text: string; type: string; btnType: string; linkTo?: string }[] | undefined;
-  listClassnameProps?: string;
-  btnClassnameProps?: string;
-}
