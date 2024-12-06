122ecec7af49c95c584020e0b1db381fc5c3e483
Stepan Skichko <st.skichko@gmail.com>
2024-12-06 13:17:01 +0200

    delete usseless sanity schemas

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,14 +1,8 @@
-import page from './page-schema';
-import project from './project-schema';
 import dishes from './dishes';
 import snacks from './snacks';
 import salads from './salads';
 import desserts from './desserts';
 import drinks from './drinks';
-
-import { eventType } from './eventType';
-import { artistType } from './artistType';
-import { venueType } from './venueType';
 
 const schemas = [dishes, snacks, salads, desserts, drinks];
 
